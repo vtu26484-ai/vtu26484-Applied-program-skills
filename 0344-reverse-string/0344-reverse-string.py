affd54a4c2256c13531a61edad6ff3f62e9ff712
vtu26484-ai <vtu26484@veltech.edu.in>
2026-02-06 14:04:43 +0530

    Time: 6 ms (20.69%), Space: 19.9 MB (70.9%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseString(self, s):
+        """
+        Reverses the list of characters in-place.
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
